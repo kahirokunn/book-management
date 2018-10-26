ba238e7f78d5cce14622a5e48d458180cc644ab0
k-okina <okinakahiro@gmail.com>
2018-10-26 13:18:46 +0900

    [add] vue-auto-routing導入

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import routes from 'vue-auto-routing';
+import { createRouterLayout } from 'vue-router-layout';
 
 Vue.use(Router);
 
+const RouterLayout = createRouterLayout((layout) => {
+  return import('@/layouts/' + layout + '.vue');
+});
+
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: RouterLayout,
+      children: routes,
     },
   ],
 });
