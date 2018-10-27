29160bb7a8f75bec7c51490df838e76b3352397e
k-okina <okinakahiro@gmail.com>
2018-10-28 03:07:31 +0900

    [add] markdownコンポーネント作成

---
+++ src/components/molecules/markdown.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+import markdown from './markdown/markdown';
+export default markdown;
+</script>
+
+<style src="./markdown/markdown.scss" scoped>
+</style>
---
+++ src/components/molecules/markdown/markdown.ts
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import marked, { Renderer } from 'marked';
+import highlight from 'highlight.js';
+import 'highlight.js/styles/github.css';
+import 'github-markdown-css/github-markdown.css';
+
+function toMarkdown(source: string) {
+  const markedOptions = {
+    langPrefix: 'hljs language-',
+    renderer: new Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: (code: string) => highlight.highlightAuto(code).value,
+  };
+
+  return marked(source, markedOptions);
+}
+
+export default Vue.extend({
+  name: 'Markdown',
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    source: {
+      type: String,
+      default: '',
+    },
+  },
+  render(h) {
+    const options = {
+      class: {
+        'markdown-body': true,
+      },
+      domProps: {
+        innerHTML: toMarkdown(this.source),
+      },
+    };
+    return h(this.tag, options);
+  },
+});
