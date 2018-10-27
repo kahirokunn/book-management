a106b46a8cd6d0e306898d0376f1bbb11f01845e
k-okina <okinakahiro@gmail.com>
2018-10-28 02:14:04 +0900

    [add] サービスロケータ作成

---
+++ src/serviceLocator/UserApplicationService.ts
@@ -0,0 +1,9 @@
+import IUserApplicationService from '@/boundaryInterface/userApplicationService/IUserApplicationService';
+import TYPES from '@/inversify/types';
+import {diContainer} from '@/inversify/config';
+
+export default class UserApplicationService {
+  public static getInstance(): IUserApplicationService {
+    return diContainer.get<IUserApplicationService>(TYPES.IUserApplicationService);
+  }
+}
