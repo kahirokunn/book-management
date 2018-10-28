7cc950d5489ec2b0f153e6605c06d40aa8dabaa8
k-okina <okinakahiro@gmail.com>
2018-10-29 02:24:22 +0900

    [add] リファクタリング

--- src/domain/model/user/FirebaseUserRepository.ts
+++ src/domain/model/user/FirebaseUserRepository.ts
@@ -1,7 +1,21 @@
+import * as Pring from 'pring';
 import {injectable} from 'inversify';
 import {auth} from '@/firebase/index';
-import User from './User';
 import IUserRepository, {ISaveParams, ICreateParams} from '@/domain/model/user/IUserRepository';
+import {IUser} from '@/boundaryInterface/userApplicationService/IUser';
+
+class User extends Pring.Base implements IUser {
+  @Pring.property public displayName!: string;
+  @Pring.property public iconFilepath!: string;
+  @Pring.property public emailAddress!: string;
+  @Pring.property public firstName!: string;
+  @Pring.property public familyName!: string;
+  @Pring.property public firstNameKana!: string;
+  @Pring.property public familyNameKana!: string;
+  @Pring.property public birthday!: Date;
+  @Pring.property public hireDate!: Date;
+  @Pring.property public gender!: Gender;
+}
 
 @injectable()
 export default class FirebaseUserRepository implements IUserRepository {
--- src/domain/model/user/IUserRepository.ts
+++ src/domain/model/user/IUserRepository.ts
@@ -1,12 +1,11 @@
 import {IUser, IUserRegistration} from '@/boundaryInterface/userApplicationService/IUser';
-import User from '@/domain/model/user/User';
 
 export interface ISaveParams extends IUser {}
 
 export interface ICreateParams extends IUserRegistration {}
 
 export default interface IUserRepository {
-  create(params: ICreateParams): Promise<User>;
-  save(params: ISaveParams): Promise<User>;
-  findById(id: Identifier): Promise<User>;
+  create(params: ICreateParams): Promise<IUser>;
+  save(params: ISaveParams): Promise<IUser>;
+  findById(id: Identifier): Promise<IUser>;
 }
