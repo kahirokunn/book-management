699796562d3b5d42eae3bc5c3d451d65550ac73d
k-okina <okinakahiro@gmail.com>
2018-11-15 22:27:54 +0900

    [add] login container full test

--- src/components/containers/loginForm/index.spec.ts
+++ src/components/containers/loginForm/index.spec.ts
@@ -1,8 +1,23 @@
 import '@/store/resolveTestConfiguration'
+import flushPromises from 'flush-promises'
+import authGetters from '@/store/middleware/auth/getters'
 import { shallowMount } from '@vue/test-utils'
+import getters from '@/store/containers/loginForm/getters'
 import index from './index.vue'
 
-test('login form container', () => {
+test('login form container', async () => {
   const wrapper = shallowMount(index)
   expect(wrapper.html()).toMatchSnapshot()
+  expect(getters.isFailed()).toBe(false)
+  expect(getters.isSending()).toBe(false)
+  const vm = wrapper.vm as any
+  vm.login({
+    email: 'tanaka@gmail.com',
+    password: 'tanakatarou',
+  })
+  expect(getters.isSending()).toBe(true)
+  expect(authGetters.user()).toBeUndefined()
+  await flushPromises()
+  expect(authGetters.user()).not.toBeUndefined()
+  expect(getters.isFailed()).toBe(false)
 })
