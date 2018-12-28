1e7b03606a45dc9f3281dc2ee6b3337ca1864990
k-okina <okinakahiro@gmail.com>
2018-12-28 17:54:23 +0900

    [add] slotを正方形にしてくれるatomを作成

---
+++ src/components/atoms/square.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="square">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class Square extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+.square {
+  position: relative;
+  height: 100%;
+}
+
+.square::before {
+  content: "";
+  display: block;
+  padding-top: 75%;
+}
+
+/deep/ > * {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+</style>
