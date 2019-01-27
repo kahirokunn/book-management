6a7ce2b9a52218d02ff4fb80e63e2e88af51f44d
k-okina <okinakahiro@gmail.com>
2019-01-27 13:26:34 +0900

    [add] changeUserProfileFormのテストを追加

---
+++ src/store/containers/changeUserProfileForm/failureUpdateProfile.spec.ts
@@ -0,0 +1,21 @@
+import '@/store/rejectTestConfiguration'
+import flushPromises from 'flush-promises'
+
+import {updateProfile} from './boundaryAction'
+import {userFactory} from '@/stub/domain/factory/IUser'
+import getters from './getters'
+import store from '@/store/root'
+
+test('ユーザープロフィールの更新に失敗する', async () => {
+  expect(getters.isSendFailed()).toBe(false)
+  expect(getters.isSending()).toBe(false)
+  expect(getters.isSendSuccess()).toBe(false)
+
+  store.commit(updateProfile({user: userFactory()}))
+  expect(getters.isSending()).toBe(true)
+  await flushPromises()
+
+  expect(getters.isSending()).toBe(false)
+  expect(getters.isSendFailed()).toBe(true)
+  expect(getters.isSendSuccess()).toBe(false)
+})
---
+++ src/store/containers/changeUserProfileForm/successUpdateProfile.spec.ts
@@ -0,0 +1,19 @@
+import '@/store/resolveTestConfiguration'
+import flushPromises from 'flush-promises'
+
+import {updateProfile} from './boundaryAction'
+import {userFactory} from '@/stub/domain/factory/IUser'
+import getters from './getters'
+import store from '@/store/root'
+
+test('ユーザープロフィールの更新に成功する', async () => {
+  expect(getters.isSendFailed()).toBe(false)
+  expect(getters.isSending()).toBe(false)
+  expect(getters.isSendSuccess()).toBe(false)
+  store.commit(updateProfile({user: userFactory()}))
+  expect(getters.isSending()).toBe(true)
+  await flushPromises()
+  expect(getters.isSending()).toBe(false)
+  expect(getters.isSendFailed()).toBe(false)
+  expect(getters.isSendSuccess()).toBe(true)
+})
