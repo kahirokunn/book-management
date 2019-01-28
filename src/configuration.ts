05308cbad335be962826c59016fdb1186607add1
k-okina <okinakahiro@gmail.com>
2019-01-28 17:25:52 +0900

    [add] start debug setting

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Router from 'vue-router'
+import {debugSetting} from 'typescript-fsa-vuex'
 
 import {auth} from '@/firebase/index'
 
@@ -8,9 +9,17 @@ import '@/firebase/index'
 import '@/plugins/vuetify'
 import {serviceContainer} from '@/inversify/config'
 import {firebaseProviders} from '@/inversify/firebaseProviders'
+import {isProd} from './submodules/env'
 
 Vue.use(Router)
 Vue.use(Vuex)
 
 auth().setPersistence(auth.Auth.Persistence.LOCAL)
 firebaseProviders(serviceContainer)
+
+if (!isProd()) {
+  debugSetting({
+    doNotUseSamePrefix: true,
+    doNotCreateSameFluxType: true,
+  })
+}
