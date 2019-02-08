1d643ca83ed1c59abf701d3225c2eb95e5a54c06
k-okina <okinakahiro@gmail.com>
2019-02-08 12:48:43 +0900

    [add] https://github.com/Microsoft/TypeScript/pull/27861/files を参考に型を修正

--- src/submodules/store/index.ts
+++ src/submodules/store/index.ts
@@ -5,15 +5,12 @@ type Store = ReturnType<typeof createStore>
 type UD = {[key: string]: (state: Store['state']) => any}
 type R<T extends UD> = {[P in keyof T]: () => ReturnType<T[P]>}
 
-export function mapComputed<T extends UD>(ud1: T): R<T>
-export function mapComputed<T extends UD, U extends UD>(ud1: T, ud2: U): R<T> & R<U>
-export function mapComputed<T extends UD, U extends UD, V extends UD>(ud1: T, ud2: U, ud3: V): R<T> & R<U> & R<V>
-
 /**
  * inject root state to mapped functions
  */
-export function mapComputed<T extends UD>(...unresolvedDictArray: T[]): {[P in keyof T]: () => ReturnType<T[P]>} {
-  const dict: any = unresolvedDictArray.reduce((prev, unresolvedDict) => Object.assign(prev, unresolvedDict) , {})
+// tslint:disable-next-line max-line-length
+export function mapComputed<T extends UD[]>(...sources: T): T extends Array<infer I> ? (I extends UD ? (a: R<I>) => void : never) extends (a: infer U) => void ? U : never : never {
+  const dict: any = sources.reduce((prev, source) => Object.assign(prev, source) , {})
   return Object
     .keys(dict)
     .reduce((prev, key) => Object.assign(prev, {
