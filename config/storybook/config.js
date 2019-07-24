e12b96006e962779a76ac6f63ec6be1feaa7aeec
k-okina <okinakahiro@gmail.com>
2019-07-25 04:30:57 +0900

    [add] storybookにpageをmountした

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,5 +1,40 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@storybook/vue'
+import {
+  addDecorator
+} from '@storybook/vue'
+import {
+  configure
+} from '@storybook/vue'
+import '@/testConfiguration'
+import "vuetify/dist/vuetify.css"
+import Router from 'vue-router'
+import {
+  storeModuleProvider
+} from '@/provider/storeModuleProvider'
+import {
+  resolveStubProviders
+} from '@/provider/resolveStubProviders'
+
+import {
+  Container
+} from 'inversify'
+import {
+  createStore,
+  ClassBasedStoreOption
+} from '@/store/root'
+
+addDecorator(() => {
+  const container = new Container()
+  resolveStubProviders(container)
+  storeModuleProvider(container)
+  const store = createStore(container.get(ClassBasedStoreOption))
+
+  return {
+    template: "<v-app><story/></v-app>",
+    store,
+    router: new Router()
+  }
+})
 
 const req = require.context('../../src/stories', true, /.stories.js$/)
 
